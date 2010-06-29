a156dcb5c516f1de1be0e18a2262eecbbc8e6a33
Tom <Tom@TomsMac.local>
2010-06-29 07:40:27 -0600

    pull bills from db

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,6 +1,5 @@
 class BillsController < ApplicationController
   def index
-    feed = Feedzirra::Feed.fetch_raw("http://www.govtrack.us/congress/billsearch_api.xpd?q=blind")
-    @results = Feedzirra::Parser::Govtrack.parse(feed).search_results
+    @bills = Bill.all
   end
 end

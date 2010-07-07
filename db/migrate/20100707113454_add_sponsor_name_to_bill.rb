9cc4cbe34b7772b673f12271d4c339f276065a9a
Tom Grushka <tom@grushka.com>
2010-07-07 08:55:51 -0600

    fix sponsor migration

--- db/migrate/20100707113454_add_sponsor_name_to_bill.rb
+++ db/migrate/20100707113454_add_sponsor_name_to_bill.rb
@@ -1,6 +1,9 @@
 class AddSponsorNameToBill < ActiveRecord::Migration
   def self.up
     add_column :bills, :sponsor_name, :string
+    
+    Bill.reset_column_information
+    Bill.all.each { |b| b.update_attribute(:sponsor_name, b.sponsor.last_name) }
   end
 
   def self.down

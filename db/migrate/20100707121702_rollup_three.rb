2359d3c205ee1d76b082cfb7a253666010153728
Tom Grushka <tom@grushka.com>
2010-07-07 11:33:52 -0600

    migration rollup #3

--- db/migrate/20100707121702_rollup_three.rb
+++ db/migrate/20100707121702_rollup_three.rb
@@ -1,11 +1,12 @@
-class RollupOne < ActiveRecord::Migration
+class AddCountsToLegislator < ActiveRecord::Migration
   def self.up
+    # migration rollup 3
+    # from schema.rb version 20100707121702
     create_table "bills", :force => true do |t|
       t.string   "drumbone_id"
       t.integer  "congress"
       t.string   "bill_type"
       t.integer  "bill_number"
-      t.string   "link"
       t.datetime "created_at"
       t.datetime "updated_at"
       t.string   "short_title"
@@ -15,19 +16,19 @@ class RollupOne < ActiveRecord::Migration
       t.date     "last_action_on"
       t.text     "last_action_text"
       t.date     "enacted_on"
-      t.float    "average_rating",     :default => 0.0,  :null => false
-      t.datetime "deleted_at"
-      t.integer  "deleted_by"
-      t.integer  "cosponsors_count",   :default => 0
+      t.float    "average_rating",          :default => 0.0,   :null => false
+      t.integer  "cosponsors_count",        :default => 0
       t.string   "govtrack_id"
       t.text     "bill_html"
-      t.boolean  "dirty",              :default => true, :null => false
       t.integer  "summary_word_count"
       t.integer  "text_word_count"
       t.integer  "blind_count"
       t.integer  "deafblind_count"
       t.string   "state"
       t.date     "text_updated_on"
+      t.integer  "visually_impaired_count"
+      t.boolean  "hidden",                  :default => false
+      t.string   "sponsor_name"
     end
 
     add_index "bills", ["drumbone_id"], :name => "index_bills_on_guid"
@@ -43,10 +44,6 @@ class RollupOne < ActiveRecord::Migration
       t.integer  "commentable_id"
       t.string   "commentable_type"
       t.text     "content"
-      t.string   "user_ip"
-      t.string   "user_agent"
-      t.string   "referer"
-      t.boolean  "approved",         :default => false, :null => false
       t.integer  "position"
       t.datetime "created_at"
       t.datetime "updated_at"
@@ -56,6 +53,7 @@ class RollupOne < ActiveRecord::Migration
       t.string   "name"
       t.datetime "created_at"
       t.datetime "updated_at"
+      t.string   "code"
     end
 
     create_table "legislators", :force => true do |t|
@@ -71,9 +69,19 @@ class RollupOne < ActiveRecord::Migration
       t.string   "party"
       t.datetime "created_at"
       t.datetime "updated_at"
+      t.integer  "sponsored_count",   :default => 0
+      t.integer  "cosponsored_count", :default => 0
     end
 
     add_index "legislators", ["bioguide_id"], :name => "index_legislators_on_bioguide_id"
+
+    create_table "pages", :force => true do |t|
+      t.string   "title"
+      t.string   "permalink"
+      t.text     "content"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
 
     create_table "posts", :force => true do |t|
       t.integer  "user_id"
@@ -82,12 +90,6 @@ class RollupOne < ActiveRecord::Migration
       t.datetime "created_at"
       t.datetime "updated_at"
       t.integer  "position"
-      t.boolean  "approved",   :default => true, :null => false
-      t.string   "user_ip"
-      t.string   "user_agent"
-      t.string   "referer"
-      t.datetime "deleted_at"
-      t.integer  "deleted_by"
     end
 
     create_table "posts_tags", :id => false, :force => true do |t|
@@ -119,6 +121,7 @@ class RollupOne < ActiveRecord::Migration
       t.datetime "deleted_at"
       t.datetime "created_at"
       t.datetime "updated_at"
+      t.integer  "post_id"
     end
 
     add_index "suggestions", ["kind"], :name => "index_suggestions_on_kind"
@@ -161,11 +164,46 @@ class RollupOne < ActiveRecord::Migration
       t.string   "time_zone"
       t.integer  "country_id"
       t.integer  "state_id"
-      t.boolean  "author",              :default => false, :null => false
+      t.boolean  "author",              :default => false,     :null => false
+      t.string   "stylesheet",          :default => "default"
+    end
+
+    create_table "versions", :force => true do |t|
+      t.integer  "versioned_id"
+      t.string   "versioned_type"
+      t.integer  "user_id"
+      t.string   "user_type"
+      t.string   "user_name"
+      t.text     "changes"
+      t.integer  "number"
+      t.string   "tag"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+
+    add_index "versions", ["created_at"], :name => "index_versions_on_created_at"
+    add_index "versions", ["number"], :name => "index_versions_on_number"
+    add_index "versions", ["tag"], :name => "index_versions_on_tag"
+    add_index "versions", ["user_id", "user_type"], :name => "index_versions_on_user_id_and_user_type"
+    add_index "versions", ["user_name"], :name => "index_versions_on_user_name"
+    add_index "versions", ["versioned_id", "versioned_type"], :name => "index_versions_on_versioned_id_and_versioned_type"
+
+    say_with_time "Running rake seed_countries..." do
+      Rake::Task["seed_countries"].invoke
+    end
+    say "DB contains #{Country.count} countries and #{State.count} states.", true
+    
+    say_with_time "Creating default homepage..." do
+      Page.create!(:title => "Homepage", :permalink => "home", :content => "Default home page.")
+    end
+    
+    say_with_time "Creating Bills..." do
+      Bill.create_from_feed
     end
   end
-
+  
   def self.down
+    drop_table :versions
     drop_table :users
     drop_table :tags
     drop_table :suggestions
@@ -173,6 +211,7 @@ class RollupOne < ActiveRecord::Migration
     drop_table :ratings
     drop_table :posts_tags
     drop_table :posts
+    drop_table :pages
     drop_table :legislators
     drop_table :countries
     drop_table :comments
